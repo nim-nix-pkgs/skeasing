65329ed4ad90ad0cdea830970e089c601947fd32
hugosenari <hugosenari@gmail.com>
2022-03-12 04:10:35 -0300

    chore: re index skeasing

---
+++ master/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = ''A collection of easing curves for animation purposes.'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-skEasing-master.flake = false;
+  inputs.src-skEasing-master.owner = "Skrylar";
+  inputs.src-skEasing-master.ref   = "refs/heads/master";
+  inputs.src-skEasing-master.repo  = "skEasing";
+  inputs.src-skEasing-master.type  = "github";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-skEasing-master"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-skEasing-master";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
